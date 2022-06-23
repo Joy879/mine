7c8e30c6dd110406d5108c66659dc74eedf46de4
Joy Wanjiru <70502261+Joy879@users.noreply.github.com>
2022-06-23 15:56:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
 
 st.write("Hello World!")
+df = px.data.gapminder()
+
+fig  = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
+           size="pop", color="continent", hover_name="country",
+           log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
+
+st.plotly_chart(fig)
+st.balloons
